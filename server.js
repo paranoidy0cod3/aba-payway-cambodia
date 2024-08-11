9b85b2701fa38f58077629a0867e724f0f968c91
paranoidy0cod3 <35105531+paranoidy0cod3@users.noreply.github.com>
2024-08-11 15:06:23 +0700

    footer added index.html

--- server.js
+++ server.js
@@ -37,37 +37,41 @@ app.post("/checkout", (req, res) => {
   const req_time = Math.floor(Date.now() / 1000);
   const tran_id = req_time;
   const merchant_id = ABA_PAYWAY_MERCHANT_ID;
-  const amount = "11.00";
-  const firstName = "Arun";
-  const lastName = "Suosdey";
-  const phone = "012345678";
-  const email = "develop@ababankapi.com";
-  const return_params = "Hello paranoidy0cod3";
+  const amount = "5.00";
+  const firstName = "Makara";
+  const lastName = "Prom";
+  const phone = "093630466";
+  const email = "prom.makara@ababank.com";
+  const return_params = "Hello World!";
   const type = "purchase";
   const currency = "USD";
   const payment_option = "abapay";
   const shipping = "1.50";
+  const cancel_url = "http://localhost:3000/cart/";
+  const continue_success_url = "http://localhost:3000/";
 
-  const dataToHash = `${req_time}${merchant_id}${tran_id}${amount}${items}${shipping}${firstName}${lastName}${email}${phone}${type}${payment_option}${currency}${return_params}`;
+  const dataToHash = `${req_time}${merchant_id}${tran_id}${amount}${items}${shipping}${firstName}${lastName}${email}${phone}${type}${continue_success_url}${currency}${return_params}`;
   const hash = generateHash(dataToHash, ABA_PAYWAY_API_KEY);
   res.send(
     `
     <form method="POST" action="${ABA_PAYWAY_API_URL}" id="aba_merchant_request">
+    <input type="hidden" name="hash" value="${hash}" />
+    <input type="hidden" name="tran_id" value="${tran_id}" />
+    <input type="hidden" name="amount" value="${amount}" />
     <input type="hidden" name="req_time" value="${req_time}" />
     <input type="hidden" name="merchant_id" value="${ABA_PAYWAY_MERCHANT_ID}" />
-    <input type="hidden" name="tran_id" value="${tran_id}" />
     <input type="hidden" name="firstname" value="${firstName}" />
     <input type="hidden" name="lastname" value="${lastName}" />
-    <input type="hidden" name="email" value="${email}" />
     <input type="hidden" name="phone" value="${phone}" />
-    <input type="hidden" name="amount" value="${amount}" />
-    <input type="hidden" name="type" value="${type}" />
-    <input type="hidden" name="payment_option" value="${payment_option}" />
+    <input type="hidden" name="email" value="${email}" />
     <input type="hidden" name="items" value="${items}" />
-    <input type="hidden" name="currency" value="${currency}" />
+    <input type="hidden" name="return_params" value="${return_params}" />    
     <input type="hidden" name="shipping" value="${shipping}" />
-    <input type="hidden" name="return_param" value="${return_params}" />    
-    <input type="hidden" name="hash" value="${hash}" />
+    <input type="hidden" name="payment_option" value="" />
+    <input type="hidden" name="type" value="${type}" />    
+    <input type="hidden" name="continue_success_url" value="${continue_success_url}" />
+    <input type="hidden" name="currency" value="${currency}" />
+    <input type="hidden" name="return_param" value="${return_params}" />
     </form>
     <script>
     document.getElementById('aba_merchant_request').submit();
